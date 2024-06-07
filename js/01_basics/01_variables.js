b3e7b0b941fcb3ed962b3feaad8c92b00645e14d
Harshil Valiya <harshilvaliya40@gmail.com>
2024-06-07 22:21:57 +0530

    variables

---
+++ js/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 139;
+let accountEmail = "harshilvaliya.work@gmail.com";
+var accountPassword = "12345";
+accountCity = "Mahuva"; //always define variables
+let accountState;
+
+// accountId = 103; // not allowed
+accountEmail = "hv@gmail.com";
+accountPassword = "123";
+accountCity = "Gandhinagar";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*  
+do not use var cause of issues of block space.
+
+ */
