b673f3846c9f01ec9fff691a9b31c80857e08245
Harshil Valiya <harshilvaliya40@gmail.com>
2024-06-11 12:24:40 +0530

    string

--- js/01_basics/05_strings.js
+++ js/01_basics/05_strings.js
@@ -45,3 +45,17 @@ console.log("----------------------------------------------------------------");
 const str = "harshil-valiya-vercel app";
 console.log(str.split("-"));
 console.log(str.split(" "));
+
+console.log("----------------------------------------------------------------");
+// comparison
+
+let a = 10;
+let b = 2;
+
+if (a < b) {
+  console.log(`${a} is less than ${b}`);
+} else if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} is equal to ${b}`);
+}
