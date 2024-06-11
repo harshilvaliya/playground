e4eb3e24d46beda73a2f18d298134a2142715899
Harshil Valiya <harshilvaliya40@gmail.com>
2024-06-11 12:16:54 +0530

    sting

---
+++ js/01_basics/05_strings.js
@@ -0,0 +1,47 @@
+// simple string concatenation.
+const name = "Batman";
+const age = 30;
+
+console.log(name + " is " + age + " years old."); // outdated
+console.log(`${name} is ${age} years old.`); // modern way to write
+
+console.log("----------------------------------------------------------------");
+
+// other way to define strings
+const heroName = new String("Superman");
+console.log(heroName);
+console.log(heroName[4]);
+console.log(heroName.__proto__);
+console.log(heroName.length);
+console.log(heroName.toUpperCase()); //
+console.log(heroName.charAt(5));
+console.log(heroName.indexOf("a"));
+
+console.log("----------------------------------------------------------------");
+
+const subString = heroName.substring(0, 5);
+console.log(subString);
+
+console.log("----------------------------------------------------------------");
+
+const stringSlice = heroName.slice(-7, 3);
+console.log(stringSlice);
+
+console.log("----------------------------------------------------------------");
+
+const spaceString = "       peacemaker  ";
+console.log(spaceString);
+console.log(spaceString.trim());
+
+console.log("----------------------------------------------------------------");
+
+const url = "http://harshil%20valiya.com";
+console.log(url.replace("%20", "-"));
+console.log(url.includes("harshil"));
+console.log(url.includes("vercel"));
+
+console.log("----------------------------------------------------------------");
+
+const str = "harshil-valiya-vercel app";
+console.log(str.split("-"));
+console.log(str.split(" "));
