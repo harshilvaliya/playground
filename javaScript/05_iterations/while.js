79134bdf63002169141e30981b4b6690f017b55e
Harshil Valiya <harshilvaliya40@gmail.com>
2024-07-01 15:00:40 +0530

    while

---
+++ javaScript/05_iterations/while.js
@@ -0,0 +1,26 @@
+// while
+
+i = 0;
+while (i <= 10) {
+  console.log(`value: ${i}`);
+  i = i + 2;
+}
+
+console.log("----------------------------------------------------------------");
+
+myHeros = ["superman", "batman", "flash"];
+
+arr = 0;
+while (arr < myHeros.length) {
+  console.log(myHeros[arr]);
+  arr++;
+}
+
+console.log("----------------------------------------------------------------");
+// do while loop
+
+let j = 10;
+do {
+  console.log(`value is ${j}`);
+  j++;
+} while (j <= 5);
