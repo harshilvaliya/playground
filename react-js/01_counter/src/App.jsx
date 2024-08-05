487b7633f1075b469e90cceced118e3a25d47391
Harshil Valiya <harshilvaliya40@gmail.com>
2024-08-05 16:24:34 +0530

    react counter

---
+++ react-js/01_counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [message, setMessage] = useState("");
+
+  const addValue = () => {
+    if (counter <= 19) {
+      setCounter(counter + 1);
+      setMessage("");
+    } else {
+      setMessage("Upper limit reached!");
+    }
+  };
+
+  const removeValue = () => {
+    if (counter >= 1) {
+      setCounter(counter - 1);
+      setMessage("");
+    } else {
+      setMessage("Lower limit reached!");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>React Counter</h1>
+      <h2>Current Value: {counter}</h2>
+      <div className="button-group">
+        <button onClick={addValue}>Add Value</button>
+        <button onClick={removeValue}>Remove Value</button>
+      </div>
+      {message && <p className="limit-message">{message}</p>}
+    </div>
+  );
+}
+
+export default App;
