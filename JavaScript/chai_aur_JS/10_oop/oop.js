5cb0d447ae88e0eaa27357f2bf846ae9eec47f4d
Harshil Valiya <harshilvaliya40@gmail.com>
2024-09-14 22:50:28 +0530

    JS oop

---
+++ JavaScript/chai_aur_JS/10_oop/oop.js
@@ -0,0 +1,29 @@
+// object literal
+const user = {
+  username: "Thalapathy Vijay",
+  movies: 68,
+  isActing: true,
+  getDetails: function () {
+    console.log(this.username, this.movies, this.isActing);
+  },
+};
+
+user.getDetails();
+
+console.log("----------------------------------------------------------------");
+
+// constructor function
+const date = new Date(); // here new is a constructor function
+
+function Actor(name, movies, isActing) {
+  this.name = name;
+  this.movies = movies;
+  this.isActing = isActing;
+  return this;
+}
+
+const actor1 = new Actor("Suriya", 43, true);
+const actor2 = new Actor("Dhanush", 50, true);
+
+console.log(actor1);
+console.log(actor2);
