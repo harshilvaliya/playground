fbaccaa184ec27d8bf6ce5982fce5b0f6f3c2fe7
Harshil Valiya <harshilvaliya40@gmail.com>
2024-10-04 19:35:48 +0530

    cpp

---
+++ cpp/chai_aur_code/01_varconst/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score;
+    score = 100;
+
+    int highestScore = 1000;
+
+    // use const for those values which are not going to change.
+    const int userId = 240214;
+
+    cout << "highestScore = " << highestScore << "" << endl;
+    return 0;
+}
